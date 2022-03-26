292d7bee5e1e407b2102aee1298b06cb818787fe
Manav Gupta <2006468@kiit.ac.in>
2022-03-26 12:37:47 +0530

    ver4

---
+++ src/103.js
@@ -0,0 +1,76 @@
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+  const lastDay = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate(); // this will give the last date of every month
+  const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();// this will give the last date of previous month
+  const firstDayIndex = date.getDay();    // this will give index value of first day of the current month
+  const lastDayIndex = new Date(date.getFullYear(),date.getMonth() + 1,0).getDay(); // last day index of the current month
+  const nextDays = 7 - lastDayIndex - 1;  // remaining days to be filled in current month
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+  let days = "";
+  for (let x = firstDayIndex; x > 0; x--) { // this will fullfil the previous date in current calender view
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) { // all date
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      days += `<div class="today">${i}</div>`;  // add class for the current date
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {  // fulfill the after date
+    days += `<div class="next-date">${j}</div>`;
+  }
+  monthDays.innerHTML = days;  // add the inner html
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const sidebarforeHamburger = document.querySelector(".side-bar");
+console.log(hamburgerMenu);
+hamburgerMenu.addEventListener('click',()=>{
+    const hamimg = hamburgerMenu.childNodes[1];
+    if(hamimg.classList.contains("closed")){
+        hamimg.classList.remove("closed");
+        hamimg.src = "http://127.0.0.1:5500/Fly%20-to%20do%20list/images/icons8-close.svg";
+        sidebarforeHamburger.classList.add("side-bar-forSmallWidth");
+        console.log(sidebarforeHamburger.classList);
+    } else {
+        hamimg.classList.add("closed");
+        hamimg.src = "http://127.0.0.1:5500/Fly%20-to%20do%20list/images/icons8-hamburger-menu%20(1).svg";
+        sidebarforeHamburger.classList.remove("side-bar-forSmallWidth");
+        console.log(sidebarforeHamburger.classList);
+    }
+})
---
+++ src/main.js
@@ -0,0 +1,191 @@
+const btnToaddToDoList = document.querySelector(".form-to-do");
+const inputfield = document.querySelector(".form-to-do  input[type='text']")
+const to_do_list = document.querySelector(".to-do-list");
+const allTo_do_list = document.querySelector(".all-to-do-list");
+const myDayPreviousContent = document.querySelector(".temprorary-content");
+const allCheckedTodoList = document.querySelector(".checked-to-do-list");
+
+const createtodo = (takeTextFromInput)=>{
+    const listOfEveryInput = document.createElement("li");
+    const innerHtmlofList = `<span>${takeTextFromInput}</span>
+             <div class="btns-of-Every-List">
+                <button class="todobtn check"><img class="check" src="/Fly -to do list/images/icons8-circle-48.png" alt=""></button>
+                <button class="todobtn remove"><img class="remove" src="/Fly -to do list/images/icons8-trash-can-48.png" alt=""></button>
+            </div>`
+    listOfEveryInput.innerHTML = innerHtmlofList;
+    to_do_list.append(listOfEveryInput);
+    inputfield.value = "";
+}
+
+
+// rendering all todolist
+let alltodos = [];
+if(localStorage.getItem('myday') == null){
+    myDayPreviousContent.style.display = "block";
+}else {
+    myDayPreviousContent.style.display = "none";
+    alltodos = JSON.parse(localStorage.getItem('myday'));
+}
+alltodos.forEach(element => {
+    createtodo(element);
+});
+
+
+
+
+
+// rendering all checked todo
+let allcheckedtodos = [];
+if(localStorage.getItem('mydaycheckeditem') == null){
+
+}else {
+    myDayPreviousContent.style.display = "none";
+    allcheckedtodos = JSON.parse(localStorage.getItem('mydaycheckeditem'));
+}
+allcheckedtodos.forEach(element => {
+    let doneitem = document.createElement('li');
+    doneitem.innerHTML = element;
+    allCheckedTodoList.append(doneitem);
+});
+
+
+
+
+// adding todos
+btnToaddToDoList.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputText = inputfield.value;
+    if (inputText.trim() != 0) {
+        myDayPreviousContent.style.display = "none";
+        const takeTextFromInput = inputText;
+        store(takeTextFromInput);
+        createtodo(takeTextFromInput);
+    }
+   
+})
+
+
+
+// checking and removing todos
+allTo_do_list.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if (e.target.classList.contains("check")) {
+        const imageurl = e.target;
+        imageurl.style.content = "url('images/icons8-circle-48 (1).png')";
+
+        const checkedbtn = e.target.parentNode;
+        checkedbtn.style.cursor = "auto";
+
+        const textvalueOfToDo = e.target.parentNode.parentNode.previousElementSibling;
+        textvalueOfToDo.style.textDecoration = "line-through";
+        textvalueOfToDo.style.color = "#696969"
+        
+        const doneitem = e.target.parentNode.parentNode.parentNode;
+        const toremoveditemfromdone = doneitem;
+        doneitem.remove();
+        localStorage.removeItem('myday');
+
+        let allremovedtodo = document.querySelectorAll(".to-do-list li");
+        allremovedtodo.forEach(element => {
+        const todotext = element.firstChild.textContent;
+        store(todotext);
+        });
+
+        allCheckedTodoList.append(toremoveditemfromdone);
+        const checkRemoved = e.target;
+        checkRemoved.classList.remove('check');
+
+        storeinchecked(toremoveditemfromdone.innerHTML);
+        
+    }
+    if (e.target.classList.contains("remove")) {
+        const doneToDo = e.target.parentNode.parentNode.parentNode;
+        doneToDo.remove()
+        if (to_do_list.childElementCount == 0 && allCheckedTodoList.childElementCount == 0) {
+            myDayPreviousContent.style.display = "block";
+        }
+        localStorage.removeItem('myday');
+        let allremovedtodo = document.querySelectorAll(".to-do-list li");
+        allremovedtodo.forEach(element => {
+            const todotext = element.firstChild.textContent;
+            store(todotext);
+        });
+        localStorage.removeItem('mydaycheckeditem');
+        let allcheckedremovedtodo = document.querySelectorAll(".checked-to-do-list li");
+        allcheckedremovedtodo.forEach(element => {
+            const todotext = element.innerHTML;
+            storeinchecked(todotext);
+        });
+    }
+})
+
+
+
+
+
+
+function store(todotext){
+    let todos = [];
+    if(localStorage.getItem('myday') != null){
+        todos = JSON.parse(localStorage.getItem('myday'));
+    }
+    todos.push(todotext);
+    localStorage.setItem('myday',JSON.stringify(todos));
+}
+
+function storeinchecked(todotext2){
+    let checkedtodos = [];
+    if(localStorage.getItem('mydaycheckeditem') != null){
+        checkedtodos = JSON.parse(localStorage.getItem('mydaycheckeditem'));
+    }
+    checkedtodos.push(todotext2);
+    localStorage.setItem('mydaycheckeditem',JSON.stringify(checkedtodos));
+}
+
+
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const sidebarforeHamburger = document.querySelector(".side-bar");
+hamburgerMenu.addEventListener('click',()=>{
+    const hamimg = hamburgerMenu.childNodes[1];
+    if(hamimg.classList.contains("closed")){
+        hamimg.classList.remove("closed");
+        hamimg.src = "http://127.0.0.1:5500/Fly%20-to%20do%20list/images/icons8-close.svg";
+        sidebarforeHamburger.classList.add("side-bar-forSmallWidth");
+        console.log(sidebarforeHamburger.classList);
+    } else {
+        hamimg.classList.add("closed");
+        hamimg.src = "http://127.0.0.1:5500/Fly%20-to%20do%20list/images/icons8-hamburger-menu%20(1).svg";
+        sidebarforeHamburger.classList.remove("side-bar-forSmallWidth");
+        console.log(sidebarforeHamburger.classList);
+    }
+})
+
+const tochangeBackground = document.querySelector(".my-day");
+const imagefromLocalStorage = JSON.parse(localStorage.getItem('backgroundImage'));
+tochangeBackground.style.backgroundImage = "url("+imagefromLocalStorage+")"
+const sett = document.querySelector(".selectBackgroundImage");
+sett.addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log("clicked");
+    if(e.target.classList.contains("closed")){
+        themechoosing.style.display = "block";
+        e.target.classList.remove("closed");
+    } else {
+        e.target.classList.add("closed");
+        themechoosing.style.display = "none";
+    }
+})
+
+
+
+const choosingBackground = document.querySelector(".backGroundImage");
+const themechoosing = document.querySelector(".theme");
+
+choosingBackground.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const urli = e.target.getAttribute('src');
+    tochangeBackground.style.backgroundImage = "url("+urli+")"
+    localStorage.setItem('backgroundImage',JSON.stringify(urli));
+})
+
